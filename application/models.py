09cecc41d871241a81b710c59767bf99ec9b0545
StevenBourn <stevenbourn2@hotmail.com>
2021-06-21 16:23:50 +0000

    Added models for Users, Ships, Cities and Routes

--- application/models.py
+++ application/models.py
@@ -2,8 +2,24 @@
 from application import db
 
 class Users(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key = True)
     username= db.Column(db.String(50), nullable = False)
     email= db.Column(db.String(50), nullable = False)
     admin =db.Column(db.Boolean, default = False)
 
+class Ships(db.Model):
+    ship_id = db.Column(db.Integer, primary_key = True)
+    ship_name = db.Column(db.String(50), nullable = False)
+    speed = db.Column(db.Integer, nullable = False)
+    owner_id = db.Column(db.Integer, db.ForeignKey(Users.user_id))
+    route_id = db.Column(db.Integer, db.ForeignKEy(Routes.route_id))
+
+class Cities(db.Model):
+    city_id = db.Column(db.Integer, primary_key = True)
+    city_name = db.Column(db.String(50), nullable = False)
+
+class Routes(db.Model):
+    route_id = db.Column(db.Integer, primary_key = True)
+    departing_id = db.Column(db.Integer, db.ForeignKey(Cities.city_id))
+    destination_id = db.Column(db.Integer, db.ForeignKey(Cities.city_id))
+    length = db.Column(db.Integer)
