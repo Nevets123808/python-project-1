3e0b5b2e6dfce0d72225ec9da3651274516f8a24
StevenBourn <stevenbourn2@hotmail.com>
2021-06-22 16:38:28 +0000

    updated requirements.txt

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 app = Flask(__name__)
 
-app.config['SQL_ALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
 app.config['SECRET_KEY'] = getenv("SECRET_KEY")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
